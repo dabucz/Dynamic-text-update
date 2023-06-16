7b29709f787a76f1c5cd06e62cf6a7ef82f586ab
dabucz <76279444+dabucz@users.noreply.github.com>
2023-06-16 08:21:22 +0200

    Update and rename NukeOverlay.vue to TextOverlay.vue

--- src/components/TextOverlay.vue
+++ src/components/TextOverlay.vue
@@ -5,7 +5,7 @@
         <div class="nuke-overlay__slide-name"> Nuke win streak </div>
         <div class="nuke-overlay__inner" style="background-color: rgb(0, 0, 0); border-color: rgb(136, 136, 136); border-radius: 45px; border-width: 5px; color: rgb(255, 255, 255);"><!---->
           <div class="nuke-overlay-content"><div class="nuke-overlay-content__slide">
-            {{ Nuke }} / 5 </div>
+            {{ Texttt }} / 5 </div>
         </div></div>
         <img src="https://overlay.wzhub.gg/images/achievements/nukes2.svg" class="nuke-overlay__icon" style="animation-name: rotation; animation-iteration-count: infinite; animation-timing-function: linear; animation-duration: 2s;"></div>
     </div>
@@ -16,13 +16,13 @@
 export default {
   data() {
     return {
-      Nuke: "0",
+      Texttt: "0",
     }
   },
   mounted() {
     const socket = new WebSocket('ws://localhost:8000');
     socket.addEventListener('message', (event) => {
-      this.Nuke = event.data;
+      this.Texttt = event.data;
     });
   }
 }
